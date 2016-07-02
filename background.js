57498b7817808dc6284e82a3e61337dfc1c6bf15
Chris Newhouse <chris.newhouse@adroll.com>
2016-07-02 09:40:23 -0400

    added .gitignore. added comments. targeting tab id for history update

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// More detailed string pattern suffixes, stored by their common root.
 var trackers_by_root = {
     "utm_": [
         "source",
@@ -24,6 +25,7 @@ for (var root in trackers_by_root ) {
 
 // Monitor for tab updates
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // If what happened to this tab was not a url change, then we're done.
     if ( !changeInfo.url ) {
         return;
     }
@@ -31,16 +33,22 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     var tab_url = tab.url;
     var params_index = tab.url.indexOf('?');
 
+    // Go through all the pattern roots
     for (var root in regexes_by_root ) {
+        // If we see the root, then we should probably try to do some replacements
         if (tab_url.indexOf(root) > params_index) {
             var tab_url = tab_url.replace( regexes_by_root[ root ], '');
+            // If we've collapsed the URL to the point where there's an '&' against the '?'
+            // then we need to get rid of that.
             if (tab_url.charAt(params_index) === '&') {
-                tab_url = tab_url.substr(0, params_index) + '?' + tab_url.substr(params_index + 1)
+                tab_url = tab_url.substr(0, params_index) + '?' + tab_url.substr(params_index + 1);
             }
         }
     }
 
+    // If we ended up making a change to the URL, let's perform a history.pushState to change the URL
+    // without a page reload
     if (tab_url != tab.url) {
-        chrome.tabs.executeScript({code: "history.pushState(history.state, document.title, '" + tab_url + "');"});
+        chrome.tabs.executeScript(tab.id, {code: "history.pushState(history.state, document.title, '" + tab_url + "');"});
     }
 });
