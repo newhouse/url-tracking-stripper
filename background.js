7a51f9a56188f973cf8a1e83ff4a2722c815ddf3
Chris Newhouse <chris.newhouse@adroll.com>
2016-07-05 11:15:08 -0400

    adding/removing handlers as appropriate

--- background.js
+++ background.js
@@ -1,3 +1,59 @@
+function log(msg) {
+    console.log(msg);
+}
+
+// CONSTS
+var STRIPPING_METHOD_HISTORY_CHANGE = 1;
+var STRIPPING_METHOD_CANCEL_AND_RELOAD = 2;
+var STRIPPING_METHOD_BLOCK_AND_RELOAD = 3;
+
+
+var STRIPPING_METHOD_TO_USE = STRIPPING_METHOD_HISTORY_CHANGE;
+
+var STRIPPING_METHOD_DESCRIPTIONS_BY_ID = {};
+STRIPPING_METHOD_DESCRIPTIONS_BY_ID[STRIPPING_METHOD_HISTORY_CHANGE.toString()] = "History Change [speed: &uarr; privacy: &darr; permissions: &uarr;]";
+STRIPPING_METHOD_DESCRIPTIONS_BY_ID[STRIPPING_METHOD_CANCEL_AND_RELOAD.toString()] = "Cancel and Re-load [speed: &darr; privacy: &uarr; permissions: &darr;]";
+STRIPPING_METHOD_DESCRIPTIONS_BY_ID[STRIPPING_METHOD_BLOCK_AND_RELOAD.toString()] = "Block and Re-load [speed: &darr; privacy: &uarr; permissions: &uarr;]";
+
+var ADD_REMOVE_LISTENERS_BY_STRIPPING_METHOD_ID = {};
+ADD_REMOVE_LISTENERS_BY_STRIPPING_METHOD_ID[STRIPPING_METHOD_HISTORY_CHANGE.toString()] = {
+    'add': function() {
+        chrome.tabs.onUpdated.addListener(history_change_handler);
+    },
+    'remove': function() {
+        chrome.tabs.onUpdated.removeListener(history_change_handler);
+    }
+};
+ADD_REMOVE_LISTENERS_BY_STRIPPING_METHOD_ID[STRIPPING_METHOD_CANCEL_AND_RELOAD.toString()] = {
+    'add': function() {
+        chrome.tabs.onUpdated.addListener(cancel_and_reload_handler);
+    },
+    'remove': function() {
+        chrome.tabs.onUpdated.removeListener(cancel_and_reload_handler);
+    }
+};
+ADD_REMOVE_LISTENERS_BY_STRIPPING_METHOD_ID[STRIPPING_METHOD_BLOCK_AND_RELOAD.toString()] = {
+    'add': function() {
+        var filters = {
+            urls: generate_patterns_array(),
+            types: ["main_frame"]
+        }
+        var extra = ["blocking"];
+        chrome.webRequest.onBeforeRequest.addListener(block_and_reload_handler, filters, extra);
+
+    },
+    'remove': function() {
+        chrome.webRequest.onBeforeRequest.removeListener(block_and_reload_handler);
+    }
+};
+
+
+
+
+
+
+
+
 // More detailed string pattern suffixes, stored by their common root.
 var trackers_by_root = {
     "utm_": [
@@ -17,38 +73,109 @@ var trackers_by_root = {
 
 // Go through all the trackers by their root and turn them into a big regex...
 var regexes_by_root = {};
-for (var root in trackers_by_root ) {
+for (var root in trackers_by_root) {
     regexes_by_root[root] = new RegExp("([\?\&]" + root + "(" + trackers_by_root[root].join('|') + ")=[^&#]+)", "ig");
 }
 
-
-
-// Monitor for tab updates
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    // If what happened to this tab was not a url change, then we're done.
-    if ( !changeInfo.url ) {
-        return;
+function generate_patterns_array() {
+    var array = [];
+    for (var root in trackers_by_root) {
+        for( var i=0; i<trackers_by_root[root].length; i++) {
+            array.push( "*://*/*" + root + trackers_by_root[root][i] + "=*" );
+        }
     }
+    return array;
+}
 
-    var tab_url = tab.url;
-    var params_index = tab.url.indexOf('?');
+function remove_trackers_from_url(url) {
+    var params_index = url.indexOf('?');
+    if(params_index == -1) {
+        return url;
+    }
 
     // Go through all the pattern roots
     for (var root in regexes_by_root ) {
         // If we see the root, then we should probably try to do some replacements
-        if (tab_url.indexOf(root) > params_index) {
-            var tab_url = tab_url.replace( regexes_by_root[ root ], '');
+        if (url.indexOf(root) > params_index) {
+            url = url.replace( regexes_by_root[ root ], '');
             // If we've collapsed the URL to the point where there's an '&' against the '?'
             // then we need to get rid of that.
-            if (tab_url.charAt(params_index) === '&') {
-                tab_url = tab_url.substr(0, params_index) + '?' + tab_url.substr(params_index + 1);
+            if (url.charAt(params_index) === '&') {
+                url = url.substr(0, params_index) + '?' + url.substr(params_index + 1);
             }
         }
     }
+    return url;
+}
 
-    // If we ended up making a change to the URL, let's perform a history.pushState to change the URL
-    // without a page reload
-    if (tab_url != tab.url) {
-        chrome.tabs.executeScript(tab.id, {code: "history.pushState(history.state, document.title, '" + tab_url + "');"});
+
+
+
+
+
+function history_change_handler(tabId, changeInfo, tab) {
+    if (!changeInfo.url) {
+        return;
     }
-});
+    var original_url = tab.url;
+    var cleansed_url = remove_trackers_from_url(original_url);
+    if (original_url != cleansed_url) {
+        chrome.tabs.executeScript(tabId, {code: "history.pushState(history.state, document.title, '" + cleansed_url + "');"});
+    }
+}
+
+function cancel_and_reload_handler(tabId, changeInfo, tab) {
+    if (!changeInfo.url) {
+        return;
+    }
+    var original_url = tab.url;
+    var cleansed_url = remove_trackers_from_url(original_url);
+    if (original_url != cleansed_url) {
+        chrome.tabs.update(tabId, {url: cleansed_url});
+    }
+}
+
+function block_and_reload_handler(details) {
+    if (!details.url) {
+        return {};
+    }
+    var original_url = details.url;
+    var cleansed_url = remove_trackers_from_url(original_url);
+    if (original_url != cleansed_url) {
+        return { redirectUrl: cleansed_url };
+    }
+    return {};
+}
+
+function set_handlers() {
+    for(method in ADD_REMOVE_LISTENERS_BY_STRIPPING_METHOD_ID) {
+        ADD_REMOVE_LISTENERS_BY_STRIPPING_METHOD_ID[method]['remove']();
+    }
+    ADD_REMOVE_LISTENERS_BY_STRIPPING_METHOD_ID[STRIPPING_METHOD_TO_USE]['add']();
+}
+
+function restore_options() {
+    chrome.storage.sync.get({
+        'STRIPPING_METHOD_TO_USE': STRIPPING_METHOD_HISTORY_CHANGE },
+        function(items) {
+            STRIPPING_METHOD_TO_USE = items.STRIPPING_METHOD_TO_USE;
+            set_handlers();
+        }
+    );
+}
+
+
+
+
+function listen_for_messsages(message, sender) {
+    if( message.action == 'options_saved') {
+        var new_method_to_use = parseInt(message.options.stripping_method);
+        STRIPPING_METHOD_TO_USE = new_method_to_use;
+        set_handlers();
+    }
+}
+
+
+
+chrome.runtime.onMessage.addListener(listen_for_messsages);
+restore_options();
--- options.js
+++ options.js
@@ -1,17 +1,60 @@
-var options_saved_timeout = 3000;
+/**********************
+	API:
 
+		https://developer.chrome.com/extensions/storage#property-sync
+		Requires permission: storage
+
+		https://developer.chrome.com/extensions/runtime#method-sendMessage
+		Requires permission: n/a
+
+***********************/
+
+
+function log(msg) {
+	console.log(msg);
+}
+
+var options_saved_timeout = 2000;
+
+// Called to save the selected options
 function save_options() {
+
 	var stripping_method = document.getElementById('stripping_method').value;
 
 	chrome.storage.sync.set({
-		stripping_method: stripping_method
+		'STRIPPING_METHOD_TO_USE': parseInt(stripping_method)
 		}, function() {
-		// Update status to let user know options were saved.
-		var status = document.getElementById('status');
-		status.textContent = 'Options saved!';
-		setTimeout(function() {
-			status.textContent = '';
-		}, options_saved_timeout);
+			// Update status to let user know options were saved.
+			var status = document.getElementById('status');
+			status.textContent = 'Options saved!';
+			setTimeout(function() {
+				status.textContent = '';
+			}, options_saved_timeout);
+		}
+	);
+
+	chrome.runtime.sendMessage({
+			action: 'options_saved',
+			options: {
+				stripping_method: stripping_method
+			}
+		}
+	);
+
+}
+
+// Dynamically generate the options page elements
+function generate_elements() {
+	chrome.runtime.getBackgroundPage(function(bp) {
+		var option;
+		var select = document.getElementById('stripping_method');
+		for(stripping_method_id in bp.STRIPPING_METHOD_DESCRIPTIONS_BY_ID) {
+			option = document.createElement('option');
+			option.value = stripping_method_id;
+			option.innerHTML = bp.STRIPPING_METHOD_DESCRIPTIONS_BY_ID[stripping_method_id];
+			select.appendChild(option);
+		}
+		restore_options();
 	});
 }
 
@@ -19,16 +62,23 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
 	chrome.storage.sync.get({
-		stripping_method: "1"
+		'STRIPPING_METHOD_TO_USE': "1"
 		}, function(items) {
-			document.getElementById('stripping_method').value = items.stripping_method;
-		});
+			document.getElementById('stripping_method').value = items['STRIPPING_METHOD_TO_USE'];
+		}
+	);
 }
 
+// Once the page content is loaded:
 document.addEventListener('DOMContentLoaded', function() {
-	restore_options();
+	// Set the title
 	var manifest = chrome.runtime.getManifest();
 	document.getElementById('title').textContent = manifest.short_name;
+
+	// Generate the elements on the page
+	generate_elements();
+
+	// Monitor for save clicks
+	document.getElementById('save').addEventListener('click', save_options);
 });
 
-document.getElementById('save').addEventListener('click', save_options);
