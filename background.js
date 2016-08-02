705ffd1bbd898bad04b2028002330aaa35018fe9
Chris Newhouse <chris.newhouse@adroll.com>
2016-08-02 10:31:38 -0700

    spaces instead of tabs

--- background.js
+++ background.js
@@ -16,7 +16,7 @@ var STUFF_BY_STRIPPING_METHOD_ID = {};
 STUFF_BY_STRIPPING_METHOD_ID[STRIPPING_METHOD_HISTORY_CHANGE.toString()] = {
     'html': "History Change [speed: &uarr; privacy: &darr; permissions: &uarr;]",
     'add': function() {
-		// Monitor tab updates so that we may update the history/url to not contain tracking params
+        // Monitor tab updates so that we may update the history/url to not contain tracking params
         chrome.tabs.onUpdated.addListener(history_change_handler);
     },
     'remove': function() {
@@ -26,38 +26,38 @@ STUFF_BY_STRIPPING_METHOD_ID[STRIPPING_METHOD_HISTORY_CHANGE.toString()] = {
 STUFF_BY_STRIPPING_METHOD_ID[STRIPPING_METHOD_CANCEL_AND_RELOAD.toString()] = {
     'html': "Cancel and Re-load [speed: &darr; privacy: &uarr; permissions: &darr;]",
     'add': function() {
-		// Monitor tab updates so that we may cancel and re-load them without tracking params
+        // Monitor tab updates so that we may cancel and re-load them without tracking params
         chrome.tabs.onUpdated.addListener(cancel_and_reload_handler);
-		// Monitor for subsequent Navigations so that we may indicate if a change
-		// was made or not.
-		chrome.webNavigation.onCompleted.addListener(web_navigation_monitor);
+        // Monitor for subsequent Navigations so that we may indicate if a change
+        // was made or not.
+        chrome.webNavigation.onCompleted.addListener(web_navigation_monitor);
     },
     'remove': function() {
         chrome.tabs.onUpdated.removeListener(cancel_and_reload_handler);
-		chrome.webNavigation.onCompleted.removeListener(web_navigation_monitor);
+        chrome.webNavigation.onCompleted.removeListener(web_navigation_monitor);
 
     }
 };
 STUFF_BY_STRIPPING_METHOD_ID[STRIPPING_METHOD_BLOCK_AND_RELOAD.toString()] = {
     'html': "Block and Re-load [speed: &darr; privacy: &uarr; permissions: &uarr;]",
     'add': function() {
-		// We are only concerned with URLs that appear to have tracking parameters in them
-		// and are in the main frame
+        // We are only concerned with URLs that appear to have tracking parameters in them
+        // and are in the main frame
         var filters = {
             urls: generate_patterns_array(),
             types: ["main_frame"]
         }
         var extra = ["blocking"];
-		// Monitor WebRequests so that we may block and re-load them without tracking params
+        // Monitor WebRequests so that we may block and re-load them without tracking params
         chrome.webRequest.onBeforeRequest.addListener(block_and_reload_handler, filters, extra);
-		// Monitor for subsequent Navigations so that we may indicate if a change
-		// was made or not.
-		chrome.webNavigation.onCompleted.addListener(web_navigation_monitor);
+        // Monitor for subsequent Navigations so that we may indicate if a change
+        // was made or not.
+        chrome.webNavigation.onCompleted.addListener(web_navigation_monitor);
 
     },
     'remove': function() {
         chrome.webRequest.onBeforeRequest.removeListener(block_and_reload_handler);
-		chrome.webNavigation.onCompleted.removeListener(web_navigation_monitor);
+        chrome.webNavigation.onCompleted.removeListener(web_navigation_monitor);
     }
 };
 
@@ -81,7 +81,7 @@ function generate_patterns_array() {
 
 // Actually strip out the tracking codes/parameters from a URL and return the cleansed URL
 function remove_trackers_from_url(url) {
-	var url_pieces = url.split('?');
+    var url_pieces = url.split('?');
     // If no params, nothing to modify
     if (url_pieces.length === 1) {
         return url;
@@ -95,11 +95,11 @@ function remove_trackers_from_url(url) {
         }
     }
 
-	// If we've collapsed the URL to the point where there's an '&' against the '?'
-	// then we need to get rid of that.
-	while (url_pieces[1].charAt(0) === '&') {
-		url_pieces[1] = url_pieces[1].substr(1);
-	}
+    // If we've collapsed the URL to the point where there's an '&' against the '?'
+    // then we need to get rid of that.
+    while (url_pieces[1].charAt(0) === '&') {
+        url_pieces[1] = url_pieces[1].substr(1);
+    }
 
     return url_pieces[1] ? url_pieces.join('?') : url_pieces[0];
 }
@@ -120,7 +120,7 @@ function history_change_handler(tabId, changeInfo, tab) {
         return;
     }
 
-	// Returns false if we didn't replace antyhing
+    // Returns false if we didn't replace antyhing
     var cleansed_url = check_url(tab.url);
     if (cleansed_url) {
         // Execute script in that tab to update the History
@@ -128,11 +128,11 @@ function history_change_handler(tabId, changeInfo, tab) {
             code: "history.pushState(history.state, document.title, '" + cleansed_url + "');"
         });
 
-		// Save the fact that we stripped something for indication later on.
-		changeManager.storeChanges(tab.id, tab.url, cleansed_url);
-		// And then immediately indicate this fact since WebNavigation has already
-		// taken place since there's no other possible hook.
-		changeManager.indicateChange(tab.id, cleansed_url);
+        // Save the fact that we stripped something for indication later on.
+        changeManager.storeChanges(tab.id, tab.url, cleansed_url);
+        // And then immediately indicate this fact since WebNavigation has already
+        // taken place since there's no other possible hook.
+        changeManager.indicateChange(tab.id, cleansed_url);
     }
 }
 
@@ -143,13 +143,13 @@ function cancel_and_reload_handler(tabId, changeInfo, tab) {
         return;
     }
 
-	// Returns false if we didn't replace antyhing
+    // Returns false if we didn't replace antyhing
     var cleansed_url = check_url(tab.url);
     if (cleansed_url) {
         // Update the URL for that tab.
         chrome.tabs.update(tabId, {url: cleansed_url});
-		// Save the fact that we stripped something for indication later on.
-		changeManager.storeChanges(tab.id, tab.url, cleansed_url);
+        // Save the fact that we stripped something for indication later on.
+        changeManager.storeChanges(tab.id, tab.url, cleansed_url);
     }
 }
 
@@ -159,88 +159,88 @@ function block_and_reload_handler(details) {
         return {};
     }
 
-	// Returns false if we didn't replace antyhing
+    // Returns false if we didn't replace antyhing
     var cleansed_url = check_url(details.url);
     if (cleansed_url) {
-		// Save the fact that we stripped something for indication later on.
-		changeManager.storeChanges(details.tabId, details.url, cleansed_url);
-		// Redirect the browser to the cleansed URL and be done here
-		return { redirectUrl: cleansed_url };
+        // Save the fact that we stripped something for indication later on.
+        changeManager.storeChanges(details.tabId, details.url, cleansed_url);
+        // Redirect the browser to the cleansed URL and be done here
+        return { redirectUrl: cleansed_url };
     }
-	// Return an empty object, which indicates we're not blocking/redirecting this
-	// request
+    // Return an empty object, which indicates we're not blocking/redirecting this
+    // request
     return {};
 }
 
 // We may need to monitor navigation so that we can let the user know when we've
 // stripped something from a URL
 var web_navigation_monitor = function(details) {
-	//  We only care about "main_frame"
-	if (details.frameId !== 0) {
-		return;
-	}
+    //  We only care about "main_frame"
+    if (details.frameId !== 0) {
+        return;
+    }
 
-	// Let's make sure this tab exists. Have seen some issues with Chrome pre-rendering
-	// but not firing the tabs.onReplaced event, so just have to be a little
-	// cautious.
-	chrome.tabs.get(details.tabId, function(tab) {
-		// Problems are caught by the runtim.lastError mechanism
-		if (chrome.runtime.lastError) {
-			changeManager.clearTab(details.tabId);
-			return;
-		}
-		// OK, let's see if we likely stripped something from the URL of this page
-		changeManager.indicateChange(details.tabId, details.url);
-	});
+    // Let's make sure this tab exists. Have seen some issues with Chrome pre-rendering
+    // but not firing the tabs.onReplaced event, so just have to be a little
+    // cautious.
+    chrome.tabs.get(details.tabId, function(tab) {
+        // Problems are caught by the runtim.lastError mechanism
+        if (chrome.runtime.lastError) {
+            changeManager.clearTab(details.tabId);
+            return;
+        }
+        // OK, let's see if we likely stripped something from the URL of this page
+        changeManager.indicateChange(details.tabId, details.url);
+    });
 };
 
 
 var changeManager = {
-	// We'll store URL change information by tab id
-	changesByTabId: {},
+    // We'll store URL change information by tab id
+    changesByTabId: {},
 
-	// Clear URL change information for a given tab
-	clearTab: function(tabId) {
-		delete changeManager.changesByTabId[tabId];
-	},
+    // Clear URL change information for a given tab
+    clearTab: function(tabId) {
+        delete changeManager.changesByTabId[tabId];
+    },
 
-	// Check to see if it looks like we changed the URL for a tab, and update/display
-	// the pageAction if so
-	indicateChange: function(tabId, cleansedUrl) {
-		// If we have change data for a tab and the URLs appear to match, let's update the pageAction
-		if (changeManager.changesByTabId[tabId] && changeManager.changesByTabId[tabId].cleansedUrl === cleansedUrl) {
-			chrome.pageAction.show(tabId);
-			chrome.pageAction.setTitle({
-				'tabId': tabId,
-				'title': "URL changed from " + changeManager.changesByTabId[tabId].originalUrl
-			});
-			// We can pass params into the URL like any other webpage, which is useful for dynamically generating the content:
-			chrome.pageAction.setPopup({tabId: tabId, popup:"info.html?title=URL Changed&originalUrl=" + encodeURIComponent(changeManager.changesByTabId[tabId].originalUrl)});
-			// Once we've updated the pageAction, we should clear out the change for that tab
-			// so that it does not show for any subsequent navigation (re-load, etc)
-			changeManager.clearTab(tabId);
-		}
-	},
+    // Check to see if it looks like we changed the URL for a tab, and update/display
+    // the pageAction if so
+    indicateChange: function(tabId, cleansedUrl) {
+        // If we have change data for a tab and the URLs appear to match, let's update the pageAction
+        if (changeManager.changesByTabId[tabId] && changeManager.changesByTabId[tabId].cleansedUrl === cleansedUrl) {
+            chrome.pageAction.show(tabId);
+            chrome.pageAction.setTitle({
+                'tabId': tabId,
+                'title': "URL changed from " + changeManager.changesByTabId[tabId].originalUrl
+            });
+            // We can pass params into the URL like any other webpage, which is useful for dynamically generating the content:
+            chrome.pageAction.setPopup({tabId: tabId, popup:"info.html?title=URL Changed&originalUrl=" + encodeURIComponent(changeManager.changesByTabId[tabId].originalUrl)});
+            // Once we've updated the pageAction, we should clear out the change for that tab
+            // so that it does not show for any subsequent navigation (re-load, etc)
+            changeManager.clearTab(tabId);
+        }
+    },
 
-	// If we made changes to a URL for a tab, let's keep that information around so that
-	// we can show the pageAction at the appropriate time
-	storeChanges: function(tabId, originalUrl, cleansedUrl) {
-		changeManager.changesByTabId[tabId] = {
-			originalUrl: originalUrl,
-			cleansedUrl: cleansedUrl
-		};
-	}
+    // If we made changes to a URL for a tab, let's keep that information around so that
+    // we can show the pageAction at the appropriate time
+    storeChanges: function(tabId, originalUrl, cleansedUrl) {
+        changeManager.changesByTabId[tabId] = {
+            originalUrl: originalUrl,
+            cleansedUrl: cleansedUrl
+        };
+    }
 };
 
 
 
 // Function to set/unset handlers for our stripping methods
 function set_handlers() {
-	// Remove any other listeners
+    // Remove any other listeners
     for (method in STUFF_BY_STRIPPING_METHOD_ID) {
         STUFF_BY_STRIPPING_METHOD_ID[method]['remove']();
     }
-	// Add the listener the user wants
+    // Add the listener the user wants
     STUFF_BY_STRIPPING_METHOD_ID[STRIPPING_METHOD_TO_USE]['add']();
 }
 
